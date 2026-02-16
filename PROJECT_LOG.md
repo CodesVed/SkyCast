cc0f95269a72c789ae42eb5935739b7ed68260f5
Vedant Jain <ved04.dev@gmail.com>
2026-02-17 00:55:13 +0530

    Initial commit: base UI of weather home

---
+++ PROJECT_LOG.md
@@ -0,0 +1,28 @@
+# PROJECT LOG - Weather App
+
+## 1. Project Snapshot
+- Platform: Android
+- Language: Kotlin
+- Stack: REST API
+- Key Libraries: Retrofit, Gson, OpenWeather
+
+## 2. Goal
+> Developed a weather app to practice API integration, JSON parsing, and dynamic UI updates in Android.
+
+---
+
+## 3. Core Features
+- Splash Screen
+- RecyclerView
+- API Integration
+
+---
+
+## 4. Issues & Fixes
+- **Issue:**   
+  **Fix:** 
+  
+---
+
+## 5. Key Learnings
+- use of OpenWeather API
---
+++ app/build.gradle.kts
@@ -0,0 +1,59 @@
+plugins {
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "com.example.weatherapp"
+    compileSdk {
+        version = release(36) {
+            minorApiLevel = 1
+        }
+    }
+
+    defaultConfig {
+        applicationId = "com.example.weatherapp"
+        minSdk = 26
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        buildConfigField("String", "OPEN_WEATHER_API_KEY",
+            "\"${project.properties["OPEN_WEATHER_API_KEY"]}\""
+        )
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    buildFeatures {
+        viewBinding = true
+        buildConfig = true
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+    implementation(libs.gson)
+    implementation(libs.retrofit)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
